fdb410d7661186b2a7f4dae2de43cb7fbef93241
Can Vardar <ismailcanvardar@gmail.com>
2024-08-14 19:26:21 +0300

    feat: update lexer.rs

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,42 +1,78 @@
+use std::{error::Error, fmt::Display};
+
 #[derive(Default, Debug)]
 pub struct Lexer<'a> {
-    pub bytecode: &'a [u8],
+    pub bytecode: &'a str,
     pub position: u64,
-    pub read_position: usize,
-    pub ch: u8,
+    pub read_position: u64,
+    pub ch: char,
 }
 
+#[derive(Debug)]
+pub enum LexerError {
+    UnableToCreateLexer,
+    HasWhitespace,
+    EmptyChar,
+    InvalidNibble,
+}
+
+impl Display for LexerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for LexerError {}
+
 impl<'a> Lexer<'a> {
-    pub fn from_hex_string(hex_string: &'a str) -> Result<Self, String> {
-        let hex_string = hex_string.strip_prefix("0x").ok_or("Missing '0x' prefix")?;
+    pub fn new(bytecode: &'a str) -> Result<Lexer<'a>, LexerError> {
+        let bytecode = bytecode.trim();
 
-        if hex_string.len() % 2 != 0 {
-            return Err("Hex string has an odd length".into());
-        }
-
-        let bytecode: Result<Vec<u8>, String> = (0..hex_string.len())
-            .step_by(2)
-            .map(|i| u8::from_str_radix(&hex_string[i..i + 2], 16).map_err(|e| e.to_string()))
-            .collect();
-
-        Ok(Self {
-            bytecode: bytecode?,
-            ..Default::default()
-        })
-    }
-
-    pub fn from_bytes(bytecode: &'a [u8]) -> Self {
-        Self {
-            bytecode,
-            ..Default::default()
-        }
-    }
-
-    pub fn read_byte(&mut self) {
-        if self.read_position >= self.bytecode.len() {
-            self.ch = 0;
+        if bytecode.starts_with("0x") {
+            match bytecode.strip_prefix("0x") {
+                Some(result) => Ok(Self {
+                    bytecode: result,
+                    ..Default::default()
+                }),
+                None => Err(LexerError::UnableToCreateLexer),
+            }
         } else {
-            self.ch = self.bytecode[self.read_position]
+            Ok(Self {
+                bytecode,
+                ..Default::default()
+            })
         }
+    }
+
+    pub fn read_char(&mut self) {
+        if self.read_position >= self.bytecode.len() as u64 {
+            self.ch = '\0';
+        } else {
+            let idx = self.read_position as usize;
+            self.ch = self.bytecode[idx..idx + 1].chars().nth(0).unwrap();
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    pub fn next_byte(&mut self) -> Result<String, Box<dyn Error>> {
+        let first_nibble = self.ch;
+        self.read_char();
+        let second_nibble = self.ch;
+
+        if first_nibble.is_whitespace() || second_nibble.is_whitespace() {
+            return Err(Box::new(LexerError::HasWhitespace));
+        }
+
+        if first_nibble == '\0' || second_nibble == '\0' {
+            return Err(Box::new(LexerError::EmptyChar));
+        }
+
+        if !first_nibble.is_ascii_hexdigit() || !second_nibble.is_ascii_hexdigit() {
+            return Err(Box::new(LexerError::InvalidNibble));
+        }
+
+        self.read_char();
+        Ok(format!("{first_nibble}{second_nibble}"))
     }
 }
