47bee47eb2718d096c01c137dadeaadb1b7b42e7
Can Vardar <ismailcanvardar@gmail.com>
2024-08-19 16:27:25 +0300

    refactor(memory): remove Result returns

--- src/memory.rs
+++ src/memory.rs
@@ -3,8 +3,6 @@ pub struct Memory {
     pub heap: Vec<u8>,
 }
 
-enum MemoryError {}
-
 impl Memory {
     pub fn new() -> Self {
         Self { heap: vec![0; 0] }
@@ -14,7 +12,7 @@ impl Memory {
         self.heap.extend(vec![0; size]);
     }
 
-    pub unsafe fn mload(&mut self, location: usize) -> Result<[u8; 32], ()> {
+    pub unsafe fn mload(&mut self, location: usize) -> [u8; 32] {
         let extended_location = location + 32;
 
         if extended_location > self.heap.len() {
@@ -27,10 +25,10 @@ impl Memory {
 
         let ptr = self.heap.as_ptr().add(location) as *const [u8; 32];
 
-        unsafe { Ok(*ptr) }
+        unsafe { *ptr }
     }
 
-    pub unsafe fn mstore(&mut self, location: usize, data: [u8; 32]) -> Result<(), ()> {
+    pub unsafe fn mstore(&mut self, location: usize, data: [u8; 32]) {
         let extended_location = location + 32;
 
         if extended_location > self.heap.len() {
@@ -40,8 +38,6 @@ impl Memory {
         let ptr = self.heap.as_mut_ptr().add(location) as *mut [u8; 32];
 
         *ptr = data;
-
-        Ok(())
     }
 
     pub fn msize(&self) -> usize {
