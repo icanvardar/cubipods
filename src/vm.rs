5b7780331ac3cf9f0d7b5c400ccbd5619bee7fd3
Can Vardar <ismailcanvardar@gmail.com>
2024-08-20 20:59:31 +0300

    refactor(vm): update Vm struct creation

--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,8 @@
+use std::error::Error;
+
 use crate::{memory::Memory, stack::Stack, storage::Storage, Lexer};
 
+#[derive(Default)]
 pub struct Vm<'a> {
     pub stack: Stack<String>,
     pub lexer: Lexer<'a>,
@@ -7,21 +10,23 @@ pub struct Vm<'a> {
     pub storage: Storage,
 }
 
-impl<'a> Default for Vm<'a> {
-    fn default() -> Self {
-        Self {
-            stack: Stack::default(),
-            lexer: Lexer::default(),
-            memory: Memory::default(),
-            storage: Storage::default(),
-        }
-    }
-}
-
 impl<'a> Vm<'a> {
-    pub fn new() -> Self {
-        Self {
+    pub fn new(bytecode: &'a str) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            lexer: Lexer::new(bytecode)?,
             ..Default::default()
+        })
+    }
+
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        self.lexer.read_char();
+
+        while self.lexer.ch != '\0' {
+            let tmp = self.lexer.next_byte()?;
+
+            println!("{:?}", tmp);
         }
+
+        Ok(())
     }
 }
