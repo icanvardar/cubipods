09746f3e8681bda2e8374c629b3ea618567dd20a
Can Vardar <ismailcanvardar@gmail.com>
2024-08-21 01:10:50 +0300

    feat: add new opcodes to InstructionType

--- src/instruction.rs
+++ src/instruction.rs
@@ -6,6 +6,8 @@ pub struct Instruction<'a> {
     pub literal: &'a str,
 }
 
+#[derive(Debug)]
+#[repr(u8)]
 pub enum InstructionType {
     STOP = 0x00,
     ADD = 0x01,
@@ -23,39 +25,48 @@ pub enum InstructionType {
     XOR = 0x18,
     NOT = 0x19,
     BYTE = 0x1a,
-    SHL = 0x1b,
-    SHR = 0x1c,
-    SAR = 0x1d,
     KECCAK256 = 0x20,
-    ADDRESS = 0x30,
+    POP = 0x50,
+    MLOAD = 0x51,
+    MSTORE = 0x52,
+    SLOAD = 0x54,
+    SSTORE = 0x55,
+    PUSH(u8),
+    DUP(u8),
+    SWAP(u8),
 }
 
 impl FromStr for InstructionType {
     type Err = InstructionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.as_bytes() {
-            b"00" => Ok(InstructionType::STOP),
-            b"01" => Ok(InstructionType::ADD),
-            b"02" => Ok(InstructionType::MUL),
-            b"03" => Ok(InstructionType::SUB),
-            b"04" => Ok(InstructionType::DIV),
-            b"06" => Ok(InstructionType::MOD),
-            b"0a" => Ok(InstructionType::EXP),
-            b"10" => Ok(InstructionType::LT),
-            b"11" => Ok(InstructionType::GT),
-            b"14" => Ok(InstructionType::EQ),
-            b"15" => Ok(InstructionType::ISZERO),
-            b"16" => Ok(InstructionType::AND),
-            b"17" => Ok(InstructionType::OR),
-            b"18" => Ok(InstructionType::XOR),
-            b"19" => Ok(InstructionType::NOT),
-            b"1a" => Ok(InstructionType::BYTE),
-            b"1b" => Ok(InstructionType::SHL),
-            b"1c" => Ok(InstructionType::SHR),
-            b"1d" => Ok(InstructionType::SAR),
-            b"20" => Ok(InstructionType::KECCAK256),
-            b"30" => Ok(InstructionType::ADDRESS),
+        let tmp = u128::from_str_radix(s, 16).unwrap();
+        match tmp {
+            0x00 => Ok(InstructionType::STOP),
+            0x01 => Ok(InstructionType::ADD),
+            0x02 => Ok(InstructionType::MUL),
+            0x03 => Ok(InstructionType::SUB),
+            0x04 => Ok(InstructionType::DIV),
+            0x06 => Ok(InstructionType::MOD),
+            0x0a => Ok(InstructionType::EXP),
+            0x10 => Ok(InstructionType::LT),
+            0x11 => Ok(InstructionType::GT),
+            0x14 => Ok(InstructionType::EQ),
+            0x15 => Ok(InstructionType::ISZERO),
+            0x16 => Ok(InstructionType::AND),
+            0x17 => Ok(InstructionType::OR),
+            0x18 => Ok(InstructionType::XOR),
+            0x19 => Ok(InstructionType::NOT),
+            0x1a => Ok(InstructionType::BYTE),
+            0x20 => Ok(InstructionType::KECCAK256),
+            0x50 => Ok(InstructionType::POP),
+            0x51 => Ok(InstructionType::MLOAD),
+            0x52 => Ok(InstructionType::MSTORE),
+            0x54 => Ok(InstructionType::SLOAD),
+            0x55 => Ok(InstructionType::SSTORE),
+            0x5f..=0x7f => Ok(InstructionType::PUSH((tmp % 0x5f) as u8)),
+            0x80..=0x8f => Ok(InstructionType::DUP(((tmp % 0x80) + 1) as u8)),
+            0x90..=0x9f => Ok(InstructionType::SWAP(((tmp % 0x90) + 1) as u8)),
             _ => {
                 let mut array = [0; 2];
                 let bytes = s.as_bytes();
