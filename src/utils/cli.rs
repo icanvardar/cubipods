1e5df65139b0c181971cf1eaa84d182f7c9c7ee7
Can Vardar <ismailcanvardar@gmail.com>
2024-08-22 13:20:35 +0300

    test(cli): implement initialization test case

--- src/utils/cli.rs
+++ src/utils/cli.rs
@@ -27,3 +27,35 @@ impl AppBuilder for Args {
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::ffi::OsString;
+
+    use super::*;
+
+    #[test]
+    fn it_initializes_args() -> Result<(), Box<dyn Error>> {
+        let args = get_mock_args(&["cubipods", "--bytecode", "0x600160026003610101"])?;
+
+        let mut vm = args.build()?;
+
+        vm.run()?;
+
+        assert_eq!(vm.stack.is_empty(), false);
+        assert_eq!(vm.stack.length(), 4);
+        assert_eq!(vm.stack.peek().unwrap(), "0101");
+        assert_eq!(args.verbose, false);
+
+        Ok(())
+    }
+
+    // NOTE: helper function to create a mock args instance
+    fn get_mock_args<I, T>(itr: I) -> Result<Args, Box<dyn Error>>
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<OsString> + Clone,
+    {
+        return Ok(Args::try_parse_from(itr)?);
+    }
+}
