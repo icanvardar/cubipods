7b9d7e75cb3b369ea529baefc46822dfacbf702f
Can Vardar <ismailcanvardar@gmail.com>
2024-08-24 17:29:46 +0300

    test(bytes32): implement test cases

--- src/utils/bytes32.rs
+++ src/utils/bytes32.rs
@@ -74,3 +74,101 @@ impl Bytes32 {
         Ok(result[lower_limit..result.len()].to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str_to_bytes32() {
+        let result = Bytes32::from_str("8060202020").unwrap();
+
+        let mut expected = [0u8; 32];
+        expected[31] = 32;
+        expected[30] = 32;
+        expected[29] = 32;
+        expected[28] = 96;
+        expected[27] = 128;
+
+        assert_eq!(result.0[31], expected[31]);
+        assert_eq!(result.0[30], expected[30]);
+        assert_eq!(result.0[29], expected[29]);
+        assert_eq!(result.0[28], expected[28]);
+        assert_eq!(result.0[27], expected[27]);
+        assert_eq!(result.0.len(), expected.len());
+    }
+
+    #[test]
+    fn test_to_string_from_bytes32() {
+        let data = "8060202020";
+        let result = Bytes32::from_str(data).unwrap();
+        let result = result.to_string();
+
+        assert_eq!(
+            result,
+            "0000000000000000000000000000000000000000000000000000008060202020"
+        );
+    }
+
+    #[test]
+    fn test_try_into_string_from_bytes32() -> Result<(), Box<dyn Error>> {
+        let data = "8060202020";
+        let result = Bytes32::from_str(data).unwrap();
+        let result: String = result.try_into()?;
+
+        assert_eq!(
+            result,
+            "0000000000000000000000000000000000000000000000000000008060202020"
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_bool_to_bytes32() {
+        let result = Bytes32::from(true);
+
+        assert_eq!(result.0[31], 1);
+    }
+
+    #[test]
+    fn test_try_into_bool_from_bytes32() -> Result<(), Box<dyn Error>> {
+        let data = true;
+        let result = Bytes32::from(data);
+        let result: bool = result.try_into()?;
+
+        assert_eq!(result, data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_u128_to_bytes32() {
+        let data = 1024;
+        let result = Bytes32::from(data);
+
+        assert_eq!(result.0[30], 4);
+    }
+
+    #[test]
+    fn test_try_into_u128_from_bytes32() -> Result<(), Box<dyn Error>> {
+        let data = 1024;
+        let result = Bytes32::from(data);
+        let result: u128 = result.try_into()?;
+
+        assert_eq!(result, data);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_cast_with_size() -> Result<(), Box<dyn Error>> {
+        let data = "8060202020";
+        let result = Bytes32::from_str(data)?;
+        let result = result.cast_with_size(5)?;
+
+        assert_eq!(result, data);
+
+        Ok(())
+    }
+}
